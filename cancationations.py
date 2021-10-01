50077d22b039d0232f2fee02d705b8e733da4e60
appzgames <73015785+appzgames@users.noreply.github.com>
2021-10-01 16:34:03 +0530

    Update cancationations.py

--- cancationations.py
+++ cancationations.py
@@ -8,3 +8,7 @@ trying to make it happen.
 
 print(first_name + ' ' + last_name)
 print(paragraph)
+
+print("concationations Successfull")
+
+# maybe there are spelling mistakes... but nevermind this aint an english class... its a coding class. so lol
